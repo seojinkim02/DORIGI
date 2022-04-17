53e49eca4b94cede779fb5f63e634baea5b3f103
YujeongLEE <dldbwjd24@ewhain.net>
2022-04-17 12:11:04 -0700

    chore : tmp

---
+++ frontend/src/Pages/Boards.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function Boards() {
+    return (
+        <div>
+            <h1>Boards</h1>
+        </div>
+    );
+}
---
+++ frontend/src/Pages/Main.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function Main() {
+    return (
+        <div>
+            <h1>MAIN</h1>
+        </div>
+    );
+}
